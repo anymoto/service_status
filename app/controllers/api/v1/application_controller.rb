b3ac30600838751d4b0bbd57e46ac60dedcb69c7
Ana Castro <ana@castromartinez.com>
2016-04-19 13:54:11 -0500

    Remove parameter from rescue_from sentence

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::ApplicationController < ActionController::Base
-  rescue_from StandardError do |exception|
+  rescue_from StandardError do
     render json: { message: 'Internal Server Error' }, status: :internal_server_error
-  end
-
-  rescue_from ActionController::ParameterMissing do |exception|
-    render json: { exception.param => 'is required' }, status: :unprocessable_entity
   end
 
   rescue_from ArgumentError do |exception|
     render json: { error: 'Not a valid status' }, status: :unprocessable_entity
+  end
+
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { exception.param => 'is required' }, status: :unprocessable_entity
   end
 end
