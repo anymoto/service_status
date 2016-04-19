60cb9cc722ddee18006dc2400a07cb7c481d36ca
Ana Castro <ana@castromartinez.com>
2016-04-18 23:30:37 -0500

    Add exception handling

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::ApplicationController < ActionController::Base
   rescue_from ActionController::ParameterMissing do |exception|
     render json: { exception.param => 'is required' }, status: :unprocessable_entity
   end
+
+  rescue_from StandardError do |exception|
+    render json: { message: 'Internal Server Error' }, status: :internal_server_error
+  end
 end
