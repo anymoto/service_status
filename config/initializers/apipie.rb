f57fde6d51067b40dcf32bc0c139921367e8d593
Ana Castro <ana@castromartinez.com>
2016-04-19 16:24:16 -0500

    Update api docs path

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,7 @@
 Apipie.configure do |config|
   config.app_name                = "LitmusStatus"
   config.api_base_url            = "/api"
-  config.doc_base_url            = "/apipie"
+  config.doc_base_url            = "/api"
   config.app_info                = "LitmusStatus API allows to update the service current status."
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
