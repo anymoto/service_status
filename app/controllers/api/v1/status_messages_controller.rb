0ff0072e11de9802890891b36b11d77ad0844cb0
Ana Castro <ana@castromartinez.com>
2016-04-19 17:17:10 -0500

    Improve endpoint docs legibility

--- app/controllers/api/v1/status_messages_controller.rb
+++ app/controllers/api/v1/status_messages_controller.rb
@@ -1,14 +1,18 @@
 class Api::V1::StatusMessagesController < Api::V1::ApplicationController
   api :POST, '/v1/status_messages', 'Create a status'
   description 'Create status with specifed status params'
+
   param :status_message, Hash, desc: 'Status information', required: true do
-    param :status, ['UP', 'DOWN'], desc: 'Service status. First status sent to the endpoint shouldn\'t be empty. Otherwise an Unprocessable Entry exception will be thrown.'
+    param :status, ['UP', 'DOWN'],
+          desc: 'Service status. First status sent to the endpoint shouldn\'t be empty. Otherwise, an Unprocessable Entry exception will be thrown.'
     param :message, String, desc: 'Status message. It can be empty anytime.'
   end
+
   example "{'status_message': {'status': 'UP', 'message': 'Everything is ok'}}"
   example "{'status_message': {'status': 'DOWN', 'message': 'Something is wrong'}}"
   example "{'status_message': {'status': 'DOWN', 'message': ''}}"
   example "{'status_message': {'status': '', 'message': 'No changes'}}"
+
   error code: 422, desc: 'Unprocessable Entity: it happens when params are malformed or status is not a valid.'
   error code: 500, desc: 'Internal Server Error: something is wrong on the server side.'
 
