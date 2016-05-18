5b75a8fc1f6e33b0ad640ab779233f42c132313d
Ana Castro <ana@castromartinez.com>
2016-05-18 17:01:21 -0500

    Set json as default response

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  namespace :api do
+  namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       post 'status_messages'          => 'status_messages#create'
       get  'status_messages/current'  => 'status_messages#current'
