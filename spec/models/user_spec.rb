8148794151a480563c46794171d9cfdaff8db357
Ana Castro <ana@castromartinez.com>
2016-05-18 12:51:57 -0500

    Add valid token scenario

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 describe User do
+  context 'with a valid token' do
+    before do
+      @user = User.new(email: 'new_user@mail.com')
+    end
+
+    it 'creates a new user' do
+      expect{ @user.save }.to change{ User.count }.by(1)
+    end
+  end
+
   context 'when their authentication token already exists' do
     let(:previous_user) { FactoryGirl.create(:user) }
     let(:existing_token) { previous_user.authentication_token }
